b84f976e55825537a1ff3c4eacf1132af0d16537
Michael Kriese <michael.kriese@visualon.de>
2016-08-08 16:28:28 +0200

    Build package with gulp and add mathjax v2.6.1

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+let gulp = require('gulp');
+let msbuild = require("gulp-msbuild");
+let nuget = require('gulp-nuget');
+let gutil = require('gulp-util');
+let del = require('del');
+
+let pjson = require("./package.json");
+
+function mj(path) {
+    return "node_modules/mathjax" + path;
+}
+
+let files = [
+    mj("/MathJax.js"),
+    mj("/LICENSE"),
+    mj("/extensions/**/*.js"),
+    mj("/fonts/**/*.{woff,txt}"),
+    mj("/jax/**/*.js"),
+    mj("/localization/**/*.js"),
+];
+
+
+let version = pjson.version;
+
+if (gutil.env.build) {
+    version += "-" + gutil.env.build;
+}
+
+function clean(done) {
+    // You can use multiple globbing patterns as you would with `gulp.src`
+    del(['bin', "MathJax.WSP/Layouts/MathJax/**"], done);
+}
+
+gulp.task('clean', clean);
+
+gulp.task('copy', ["clean"], function () {
+    return gulp
+        .src(files, { base: mj() })
+        .pipe(gulp.dest("MathJax.WSP/Layouts/MathJax"))
+});
+
+gulp.task('build', ["copy"], function () {
+    return gulp
+        .src("MathJax.WSP/MathJax.WSP.csproj")
+        .pipe(msbuild({
+            errorOnFail: true,
+            stdout: true,
+            targets: ['Package'],
+            toolsVersion: 14.0,
+            configuration: "Release",
+            verbosity: "minimal"
+        }))
+});
+
+gulp.task('pack', ["build"], function () {
+    return gulp
+        .src("MathJax.nuspec")
+        .pipe(nuget.pack({
+            nuget: "nuget.exe",
+            version: version,
+            properties: 'configuration=Release;author=VisualOn GmbH;year=' + new Date().getUTCFullYear()
+        }))
+    .pipe(gulp.dest("bin/"))
+});
+
+gulp.task('default', ["pack"]);
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+if ( (Get-PSSnapin -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue) -eq $null )
+{
+    Add-PsSnapin Microsoft.SharePoint.PowerShell
+}
+
+$file = gci $fld -File -Filter "*.wsp" | select -First 1
+
+$sol = Get-SPSolution | ? { $_.Name -eq $file.Name }
+if ($sol -ne $null)
+{
+    #Uninstall-SPSolution $sol -AllWebApplications -Local
+    #$sol.LastOperationDetails | Write-Host -ForegroundColor Yellow
+    #Remove-SPSolution $sol
+}
+
+"You have to manually uninstall the solution: " + $file.Name | Write-Host -ForegroundColor Yellow
